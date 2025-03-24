9ed14774c2c49f3dad7a6d681e59c5e669786637
Anderson Banihirwe <13301940+andersy005@users.noreply.github.com>
2025-03-24 15:26:03 -0700

    Update .gitignore, add dependency versioning, and remove dummy test

---
+++ ocr/__init__.py
@@ -0,0 +1,3 @@
+# flake8: noqa
+from ocr._version import __version__
+from ocr.dep_versions import show_versions
---
+++ ocr/dep_versions.py
@@ -0,0 +1,53 @@
+import importlib
+import sys
+
+
+def show_versions(file=sys.stdout):
+    """print the versions of dependencies.
+       Adapted from xarray/util/print_versions.py
+
+    Parameters
+    ----------
+    file : file-like, optional
+        print to the given file-like object. Defaults to sys.stdout.
+    """
+
+    deps = [
+        ('ocr', lambda mod: mod.__version__),
+        ('xarray', lambda mod: mod.__version__),
+        ('geopandas', lambda mod: mod.__version__),
+        ('rioxarray', lambda mod: mod.__version__),
+        ('windrose', lambda mod: mod.__version__),
+        ('matplotlib', lambda mod: mod.__version__),
+        ('cv2', lambda mod: mod.__version__),
+        ('s3fs', lambda mod: mod.__version__),
+        ('scipy', lambda mod: mod.__version__),
+        ('dask', lambda mod: mod.__version__),
+        ('zarr', lambda mod: mod.__version__),
+        ('icechunk', lambda mod: mod.__version__),
+        ('shapely', lambda mod: mod.__version__),
+        ('coiled', lambda mod: mod.__version__),
+    ]
+
+    deps_blob = []
+    for modname, ver_f in deps:
+        try:
+            if modname in sys.modules:
+                mod = sys.modules[modname]
+            else:
+                mod = importlib.import_module(modname)
+        except Exception:
+            deps_blob.append((modname, None))
+        else:
+            try:
+                ver = ver_f(mod)
+                deps_blob.append((modname, ver))
+            except Exception:
+                deps_blob.append((modname, 'installed'))
+
+    print('\nINSTALLED VERSIONS', file=file)
+    print('------------------', file=file)
+
+    print('', file=file)
+    for k, stat in sorted(deps_blob):
+        print(f'{k}: {stat}', file=file)
