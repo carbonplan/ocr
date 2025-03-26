a15ed3bac73a5190fa95000a37f66f940654622b
norlandrhagen <norlandrhagen@gmail.com>
2025-03-26 16:21:43 -0600

    adds processing scripts to subset and write building and address geoparquets

---
+++ input-data/overture_vector/README.md
@@ -0,0 +1,7 @@
+## processing scripts to retrieve and subset vector building and address data from overture to CONUS.
+
+### Note: Overture data is updated frequently!
+
+### Runtime
+
+On a coiled c8g.xlarge aws machine, subsetting the building and address datasets takes about 14 and 4 minutes respectively.
---
+++ input-data/overture_vector/subset_overture_address.py
@@ -0,0 +1,21 @@
+# COILED n-tasks 1
+# COILED --region us-west-2
+# COILED --forward-aws-credentials
+# COILED --vm-type c8g.xlarge
+# COILED --tag project=OCR
+
+import duckdb
+from ocr_vector.utils import apply_s3_creds, install_load_extensions
+
+install_load_extensions()
+apply_s3_creds()
+
+release = '2025-03-19.1'
+
+# conus bbox
+bbox = (-125.354004, 24.413323, -66.555176, 49.196737)
+
+result = duckdb.sql(f"""COPY (SELECT * FROM read_parquet('s3://overturemaps-us-west-2/release/{release}/theme=addresses/type=address/*.parquet')
+WHERE
+bbox.xmin BETWEEN {bbox[0]} AND {bbox[2]} AND
+bbox.ymin BETWEEN {bbox[1]} AND {bbox[3]} ) TO 's3://carbonplan-ocr/input/fire-risk/vector/CONUS_overture_addresses_{release}.parquet'  (FORMAT 'parquet', COMPRESSION 'zstd');""")
