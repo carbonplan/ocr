b09d960895f65ba0c6d72bac159bf386d3dfaaf6
Raphael Hagen <norlandrhagen@gmail.com>
2025-06-03 16:02:21 -0600

    adds 03 reproject step for community risk USFS dataset (#50)

---
+++ input-data/tensor/USFS_fire_risk/RDS-2020-0016-2/03_reproject.py
@@ -0,0 +1,57 @@
+# Script to reproject the USFS 30m Community Risk dataset to EPSG:4326
+
+
+import coiled
+import icechunk
+import xarray as xr
+from icechunk.xarray import to_icechunk
+from odc.geo.xr import assign_crs, xr_reproject
+
+from ocr import catalog
+
+
+def load_USFS_community_risk() -> xr.Dataset:
+    rps_30 = catalog.get_dataset(
+        'USFS-wildfire-risk-communities'
+    ).to_xarray()  # downcast to float32
+    for var in list(rps_30):
+        rps_30[var] = rps_30[var].astype('float32')
+    return rps_30
+
+
+def write_to_icechunk(ds: xr.Dataset):
+    storage = icechunk.s3_storage(
+        bucket='carbonplan-ocr',
+        prefix='input/fire-risk/tensor/USFS/RDS-2022-0016-02_EPSG_4326_icechunk',
+        region='us-west-2',
+    )
+    repo = icechunk.Repository.open_or_create(storage)
+    session = repo.writable_session('main')
+    to_icechunk(ds, session)
+    session.commit('reproject to 4326')
+
+
+def interpolate_and_reproject():
+    rps_30 = load_USFS_community_risk()
+
+    # assign crs and reproject to lat/lon EPSG:4326
+    rps_30 = assign_crs(rps_30, crs='EPSG:5070')
+    rps_30_4326 = xr_reproject(rps_30, how='EPSG:4326')
+
+    # Write to icechunk
+    write_to_icechunk(rps_30_4326)
+
+
+@coiled.function(
+    region='us-west-2',
+    n_workers=10,
+    vm_type='r7a.2xlarge',
+    tags={'Project': 'OCR'},
+    idle_timeout='5 minutes',
+)
+def main():
+    interpolate_and_reproject()
+
+
+if __name__ == '__main__':
+    main()
