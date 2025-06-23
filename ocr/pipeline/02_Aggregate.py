7f2bd191fb2e1ad29a86cceb692ec6bf8fdeb68f
Raphael Hagen <norlandrhagen@gmail.com>
2025-06-23 17:09:38 -0600

    update to m8g (#60)

--- ocr/pipeline/02_Aggregate.py
+++ ocr/pipeline/02_Aggregate.py
@@ -1,7 +1,7 @@
 # COILED n-tasks 1
 # COILED --region us-west-2
 # COILED --forward-aws-credentials
-# COILED --vm-type m7a.xlarge
+# COILED --vm-type m8g.large
 # COILED --tag project=OCR
 # COILED --name Aggregate_Geoparquet
 
