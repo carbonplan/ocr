9bccf05a64493bee79ce463aa07d600630c22aa9
norlandrhagen <norlandrhagen@gmail.com>
2025-06-23 16:53:01 -0600

    update to m8g

--- ocr/pipeline/01_Write_Region.py
+++ ocr/pipeline/01_Write_Region.py
@@ -1,7 +1,7 @@
 # COILED n-tasks 1
 # COILED --region us-west-2
 # COILED --forward-aws-credentials
-# COILED --vm-type m7a.large
+# COILED --vm-type m8g.large
 # COILED --tag project=OCR
 from __future__ import annotations
 
