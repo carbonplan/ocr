930b120c6e3ab280eea915539ce7b6fe7338de02
norlandrhagen <norlandrhagen@gmail.com>
2025-06-25 11:38:40 -0600

    update data pipeline usage and add pyramid flag in batch

--- docs/usage/data_pipeline.md
+++ docs/usage/data_pipeline.md
@@ -2,22 +2,24 @@
 
 ![](../assets/ocr_data_flow.png)
 
-## main
+## Deployment / Job Orchestration
 
-#### `region_id` based atomic unit jobs
-
-`ocr/main.py` is a click CLI that can be used to dispatch parallel batch jobs of `01_write_region`.
+`ocr/deploy.py` is a click CLI coordination / orchestration script. It will launch multiple parallel jobs of `01_Write_Region.py`, monitor the jobs with blocking behavior before dispatching `02_Pyramid (non-blocking)`, `02_Aggregate (blocking)` and `03_Tiles (blocking)`.
 
 <!-- prettier-ignore-start -->
 ::: mkdocs-click
-    :module: ocr.main
-    :command: main
+    :module: ocr.deploy
+    :command: deploy
 <!-- prettier-ignore-end -->
 
 ## 01_Write_Regions
 
 1. `run_wind_region` - for a given region_id, applies wind adjustment and writes that region to an icechunk template.
 2. `sample_risk_region` - for a given region_id, samples the building centroids to the wind adjusted risk raster to get building level risks and writes those to region_specific geoparquet files.
+
+## 02_Pyramid
+
+python script that runs ndpyramid coarsen to create a single level multi-scale Zarr pyramid. Default is non-reprojecting (EPSG:4326), but has a boolean flag for reprojecting to web-mercator `-r`. Note: reprojecting uses significantly more resources, so a larger machine may be required (rec r8g or r7a series for high ram).
 
 ## 02_Aggregate
 
@@ -26,9 +28,11 @@ python script that uses duckdb spatial to aggregate region_id specific geoparque
 ## 03_Tiles
 
 bash script that uses GDAL and tippecanoe to create PMTiles from geoparquet.
+TODO: Update this to a python deployment
 
 # Example usage:
 
-- run a single region on coiled batch: `uv run python main.py -c -r y2_x4`
-- run multiple regions on coiled batch in parallel: `uv run python main.py -c -r y2_x4 -r y2_x5 -r y2_x6`
-- run a single region on prod branch on coiled batch: `uv run python main.py -c -b prod -r y2_x4`
+- run a single region: `uv run python deploy.py -r y2_x4`
+- run multiple regions in parallel: `uv run python deploy.py -r y2_x4 -r y2_x5 -r y2_x6`
+- run a single region on the prod branch: `uv run python deploy.py -b prod -r y2_x4`
+- run a single region and generate pyramids: `uv run python deploy.py -r y2_x4 -p`
---
+++ ocr/deploy.py
@@ -0,0 +1,86 @@
+import click
+
+
+@click.command(name='uv run deploy.py')
+@click.option(
+    '-r',
+    '--region-id',
+    multiple=True,  # this allows for multiple inputs: ex: uv run python deploy.py -r y10_x0 -r y0_x10
+    help="region IDs. ex: 'y10_x2'",
+)
+@click.option('--branch', '-b', default='QA', help='data branch path [QA, prod]. Default is QA')
+@click.option(
+    '--wipe',
+    '-w',
+    is_flag=True,
+    help='If True, wipes icechunk repo and vector data before initializing.',
+)
+@click.option(
+    '-p',
+    '--pyramid',
+    is_flag=True,
+    default=False,
+    help='Generate ndpyramid, default False',
+)
+@click.option(
+    '--debug',
+    is_flag=True,
+    default=False,
+    help='Enable Debugging, default False',
+)
+def main(
+    region_id: tuple[str, ...],
+    branch: str = 'QA',
+    pyramid: bool = False,
+    wipe: bool = False,
+    debug: bool = False,
+):
+    # from ocr.config import BatchJobs
+    from ocr.batch import CoiledBatchManager
+    from ocr.template import IcechunkConfig, VectorConfig
+
+    # config_init applies any wipe and re-init opts
+    IcechunkConfig(branch=branch, wipe=wipe).config_init()
+    VectorConfig(branch=branch, wipe=wipe).config_init()
+
+    # ------------- 01 AU ---------------
+
+    batch_manager_01 = CoiledBatchManager(debug=debug)
+    # region_id is tuple
+    for rid in region_id:
+        batch_manager_01.submit_job(
+            command=f'python pipeline/01_Write_Region.py -r {rid} -b {branch}',
+            name=f'process-region-{rid}-{branch}',
+        )
+
+    # this is a monitoring / blocking func. We should be able to block with this, then run 02, 03 etc.
+    batch_manager_01.wait_for_completion()
+
+    # ----------- 02 Pyramid -------------
+    # This is non-blocking, since there are no post-pyramid dependent operations, so no wait_for_completion (I think)
+    if pyramid:
+        batch_manager_pyarmid_02 = CoiledBatchManager(debug=debug)
+        batch_manager_pyarmid_02.submit_job(
+            command=f'python pipeline/02_Pyramid.py -b {branch}',
+            name=f'create-pyramid-{branch}',
+        )
+    # ----------- 02 Aggregate -------------
+    batch_manager_aggregate_02 = CoiledBatchManager(debug=debug)
+    batch_manager_aggregate_02.submit_job(
+        command=f'python pipeline/02_Aggregate.py -b {branch}',
+        name=f'aggregate-geoparuqet-{branch}',
+    )
+    batch_manager_aggregate_02.wait_for_completion()
+
+    # ------------- 03  Tiles ---------------
+    batch_manager_03 = CoiledBatchManager(debug=debug)
+    batch_manager_03.submit_job(
+        command='pipeline/03_Tiles.sh',
+        name=f'create-pmtiles-{branch}',
+    )
+    # We probably don't need this, but we do get reporting, which is kind of nice.
+    batch_manager_03.wait_for_completion()
+
+
+if __name__ == '__main__':
+    main()
