51de6cda309cb48fe6c5dce5cb39ccaeb5ad1cff
norlandrhagen <norlandrhagen@gmail.com>
2025-06-25 10:00:01 -0600

    start of blocking / monitoring coiled batch submission

--- ocr/main.py
+++ ocr/main.py
@@ -1,5 +1,3 @@
-import subprocess
-
 import click
 
 
@@ -16,7 +14,6 @@ import click
     multiple=True,  # this allows for multiple inputs: ex: uv run python ocr/main.py -r y10_x0 -r y0_x10
     help="region IDs. ex: 'y10_x2'",
 )
-@click.option('-c', '--run-on-coiled', is_flag=True, help='If True, run via coiled batch')
 @click.option('--branch', '-b', default='QA', help='data branch path [QA, prod]. Default is QA')
 @click.option(
     '--wipe',
@@ -27,23 +24,45 @@ import click
 def main(
     region_id: tuple[str, ...],
     branch: str = 'QA',
-    run_on_coiled: bool = False,
     wipe: bool = False,
     debug: bool = False,
 ):
-    from ocr.config import BatchJobs
+    # from ocr.config import BatchJobs
+    from ocr.batch import CoiledBatchManager
     from ocr.template import IcechunkConfig, VectorConfig
 
     # config_init applies any wipe and re-init opts
     IcechunkConfig(branch=branch, wipe=wipe).config_init()
     VectorConfig(branch=branch, wipe=wipe).config_init()
 
-    batch_jobs = BatchJobs(region_id, run_on_coiled=run_on_coiled)
-    batch_commands = batch_jobs.generate_batch_commands(branch=branch)
-    print(batch_commands)
-    for submit_command in batch_commands:
-        print(f'submitting: {submit_command}')
-        subprocess.Popen(submit_command, shell=True, cwd='.')
+    # ------------- 01 ---------------
+
+    batch_manger_01 = CoiledBatchManager(debug=debug)
+    # region_id is tuple
+    for rid in region_id:
+        batch_manger_01.submit_job(
+            command=f'python pipeline/01_Write_Region.py -r {rid} -b {branch}',
+            name=f'process-region-{rid}-{branch}',
+        )
+
+    # this is a monitoring / blocking func. We should be able to block with this, then run 02, 03 etc.
+    batch_manger_01.wait_for_completion()
+
+    # ------------- 02 ---------------
+    batch_manger_02 = CoiledBatchManager(debug=debug)
+    batch_manger_02.submit_job(
+        command=f'python pipeline/02_Aggregate.py -b {branch}',
+        name=f'aggregate-geoparuqet-{branch}',
+    )
+    batch_manger_02.wait_for_completion()
+
+    # ------------- 03 ---------------
+    batch_manger_03 = CoiledBatchManager(debug=debug)
+    batch_manger_03.submit_job(
+        command='pipeline/03_Tiles.sh',
+        name=f'create-pmtiles-{branch}',
+    )
+    batch_manger_03.wait_for_completion()
 
 
 if __name__ == '__main__':
