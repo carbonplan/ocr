2a0e70b13c4bcca8f052299fb82aed70d462badd
norlandrhagen <norlandrhagen@gmail.com>
2025-07-17 14:50:09 -0600

    wip on aggregated country stats

--- ocr/pipeline/create_county_stats.py
+++ ocr/pipeline/create_county_stats.py
@@ -4,8 +4,7 @@
 # COILED --vm-type m8g.8xlarge
 # COILED --tag project=OCR
 
-
-# peak 86GB
+# peak 96GB!
 
 
 import duckdb
@@ -13,13 +12,38 @@ import duckdb
 con = duckdb.connect(database=':memory:')
 con.execute("""INSTALL SPATIAL; LOAD SPATIAL; INSTALL HTTPS; LOAD HTTPFS""")
 
-hist_bins = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+hist_bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
 
 con.execute("""
 CREATE TEMP TABLE temp_buildings AS
-SELECT geometry, risk_2011, risk_2047, wind_risk_2011, wind_risk_2047
+SELECT geometry,
+
+1: annualProbability * 100,
+15: (1 - Math.pow(1 - annualProbability, 15)) * 100,
+30: (1 - Math.pow(1 - annualProbability, 30)) * 100,
+
+
+risk_2011 * 100.0 as risk_2011_horizon_1,
+(1.0 - POWER((1 - risk_2011), 15)) * 100.0 as risk_2011_horizon_15,
+(1.0 - POWER((1 - risk_2011), 30)) * 100.0 as risk_2011_horizon_30,
+
+risk_2047 * 100.0 as risk_2047_horizon_1,
+(1.0 - POWER((1 - risk_2047), 15)) * 100.0 as risk_2047_horizon_15,
+(1.0 - POWER((1 - risk_2047), 30)) * 100.0 as risk_2047_horizon_30,
+
+wind_risk_2011 * 100.0 as wind_risk_2011_horizon_1,
+(1.0 - POWER((1 - wind_risk_2011), 15)) * 100.0 as wind_risk_2011_horizon_15,
+(1.0 - POWER((1 - wind_risk_2011), 30)) * 100.0 as wind_risk_2011_horizon_30,
+
+wind_risk_2047 * 100.0 as wind_risk_2047_horizon_1,
+(1.0 - POWER((1 - wind_risk_2047), 15)) * 100.0 as wind_risk_2047_horizon_15,
+(1.0 - POWER((1 - wind_risk_2047), 30)) * 100.0 as wind_risk_2047_horizon_30,
+
+
 FROM read_parquet('s3://carbonplan-ocr/intermediate/fire-risk/vector/prod/consolidated_geoparquet.parquet')
 """)
+
 
 con.execute("""
 CREATE TEMP TABLE temp_counties AS
@@ -33,19 +57,45 @@ con.execute('CREATE INDEX counties_spatial_idx ON temp_counties USING RTREE (geo
 
 result = con.query(f"""
 COPY (SELECT b.NAME as county_name, count(b.NAME) as building_count,
-avg(a.risk_2011) as fire_risk_2011_avg,
-avg(a.risk_2047) as fire_risk_2047_avg,
-avg(a.wind_risk_2011) as fire_wind_risk_2011_avg,
-avg(a.wind_risk_2047) as fire_wind_risk_2047_avg,
 
-histogram(a.risk_2011, {hist_bins}) as risk_2011,
-histogram(a.risk_2047, {hist_bins}) as risk_2047,
-histogram(a.wind_risk_2011, {hist_bins}) as wind_risk_2011,
-histogram(a.wind_risk_2047, {hist_bins}) as wind_risk_2047,
+avg(a.risk_2011_horizon_1) as avg_risk_2011_horizon_1,
+avg(a.risk_2011_horizon_15) as avg_risk_2011_horizon_15,
+avg(a.risk_2011_horizon_30) as avg_risk_2011_horizon_30,
+
+avg(a.risk_2047_horizon_1) as avg_risk_2047_horizon_1,
+avg(a.risk_2047_horizon_15) as avg_risk_2047_horizon_15,
+avg(a.risk_2047_horizon_30) as avg_risk_2047_horizon_30,
+
+avg(a.wind_risk_2011_horizon_1) as avg_wind_risk_2011_horizon_1,
+avg(a.wind_risk_2011_horizon_15) as avg_wind_risk_2011_horizon_15,
+avg(a.wind_risk_2011_horizon_30) as avg_wind_risk_2011_horizon_30,
+
+avg(a.wind_risk_2047_horizon_1) as avg_wind_risk_2047_horizon_1,
+avg(a.wind_risk_2047_horizon_15) as avg_wind_risk_2047_horizon_15,
+avg(a.wind_risk_2047_horizon_30) as avg_wind_risk_2047_horizon_30,
+
+
+map_values(histogram(a.risk_2011_horizon_1, {hist_bins})) as risk_2011_horizon_1,
+map_values(histogram(a.risk_2011_horizon_15, {hist_bins})) as risk_2011_horizon_15,
+map_values(histogram(a.risk_2011_horizon_30, {hist_bins})) as risk_2011_horizon_30,
+
+map_values(histogram(a.risk_2047_horizon_1, {hist_bins})) as risk_2047_horizon_1,
+map_values(histogram(a.risk_2047_horizon_15, {hist_bins})) as risk_2047_horizon_15,
+map_values(histogram(a.risk_2047_horizon_30, {hist_bins})) as risk_2047_horizon_30,
+
+map_values(histogram(a.wind_risk_2011_horizon_1, {hist_bins})) as wind_risk_2011_horizon_1,
+map_values(histogram(a.wind_risk_2011_horizon_15, {hist_bins})) as wind_risk_2011_horizon_15,
+map_values(histogram(a.wind_risk_2011_horizon_30, {hist_bins})) as wind_risk_2011_horizon_30,
+
+map_values(histogram(a.wind_risk_2047_horizon_1, {hist_bins})) as wind_risk_2047_horizon_1,
+map_values(histogram(a.wind_risk_2047_horizon_15, {hist_bins}) as wind_risk_2047_horizon_15,
+map_values(histogram(a.wind_risk_2047_horizon_30, {hist_bins})) as wind_risk_2047_horizon_30,
+
+b.geometry as geometry
 
 FROM temp_buildings a
 JOIN temp_counties b ON ST_Intersects(a.geometry, b.geometry)
-GROUP BY b.NAME )
+GROUP BY b.NAME, b.geometry )
 TO 's3://carbonplan-ocr/intermediate/fire-risk/vector/county/county_summary_stats.parquet'
 (
         FORMAT 'parquet',
