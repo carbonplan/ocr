b1835499f5d7768a848d364bc803a71ee14f5210
norlandrhagen <norlandrhagen@gmail.com>
2025-08-15 13:57:26 -0600

    :shrug:

--- ocr/config.py
+++ ocr/config.py
@@ -792,19 +792,19 @@ class VectorConfig(pydantic_settings.BaseSettings):
     @functools.cached_property
     def buildings_pmtiles_uri(self) -> UPath:
         path = UPath(f'{self.storage_root}/{self.pmtiles_prefix}/buildings.pmtiles')
-        path.mkdir(parents=True, exist_ok=True)
+        path.parent.mkdir(parents=True, exist_ok=True)
         return path
 
     @functools.cached_property
     def tracts_pmtiles_uri(self) -> UPath:
         path = UPath(f'{self.storage_root}/{self.pmtiles_prefix}/tracts.pmtiles')
-        path.mkdir(parents=True, exist_ok=True)
+        path.parent.mkdir(parents=True, exist_ok=True)
         return path
 
     @functools.cached_property
     def counties_pmtiles_uri(self) -> UPath:
         path = UPath(f'{self.storage_root}/{self.pmtiles_prefix}/counties.pmtiles')
-        path.mkdir(parents=True, exist_ok=True)
+        path.parent.mkdir(parents=True, exist_ok=True)
         return path
 
     # ----------------------------
@@ -828,7 +828,7 @@ class VectorConfig(pydantic_settings.BaseSettings):
     @functools.cached_property
     def building_geoparquet_uri(self) -> UPath:
         path = UPath(f'{self.storage_root}/{self.geoparquet_prefix}/buildings.parquet')
-        path.mkdir(parents=True, exist_ok=True)
+        path.parent.mkdir(parents=True, exist_ok=True)
         return path
 
     @functools.cached_property
