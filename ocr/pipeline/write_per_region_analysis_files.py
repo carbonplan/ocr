ebb8f92a875c2793a78ef87d1641e21a6a85d711
norlandrhagen <norlandrhagen@gmail.com>
2025-10-07 13:39:20 -0600

    fix zipped files and schema

--- ocr/pipeline/write_per_region_analysis_files.py
+++ ocr/pipeline/write_per_region_analysis_files.py
@@ -24,27 +24,27 @@ def write_per_region(*, con: duckdb.DuckDBPyConnection, config: OCRConfig, regio
     region_path = UPath(f's3://{region_ds.bucket}/{region_ds.prefix}')
 
     # create joined temp table
-    con.execute(f""" CREATE TEMP TABLE {region_type}_grouped_risk AS SELECT  b.NAME, b.GEOID, a.USFS_RPS, a.wind_risk_2011, a.wind_risk_2047,  ST_X(ST_Centroid(a.geometry)) AS longitude,
+    con.execute(f""" CREATE TEMP TABLE {region_type}_grouped_risk AS SELECT  b.NAME, b.GEOID,
+                a.wind_risk_2011,
+                a.wind_risk_2047,
+                a.burn_probability_2011,
+                a.burn_probability_2047,
+                a.conditional_risk_usfs,
+                a.burn_probability_usfs_2011,
+                a.burn_probability_usfs_2047,  ST_X(ST_Centroid(a.geometry)) AS longitude,
     ST_Y(ST_Centroid(a.geometry)) AS latitude, a.geometry from read_parquet('{consolidated_buildings_uri.as_uri()}') a
     JOIN read_parquet('{region_path}') b ON ST_Intersects(a.geometry, b.geometry) """)
 
     geoid_list = con.sql(
         f"""SELECT DISTINCT(GEOID) from {region_type}_grouped_risk"""
-    ).fetchnumpy()['GEOID']  # np array
-
-    # parquet_path = per_region_output_prefix / region_type / 'parquet'
-    # parquet_path.mkdir(parents=True, exist_ok=True)
-    # # write geoparuqet
-    # for geoid in geoid_list:
-    #     con.execute(f"""COPY (SELECT * EXCLUDE latitude, longitude FROM {region_type}_grouped_risk WHERE GEOID = '{geoid}') TO '{parquet_path}/{geoid}.parquet'
-    #     (FORMAT parquet,  COMPRESSION 'zstd', OVERWRITE_OR_IGNORE);""")
+    ).fetchnumpy()['GEOID']
 
     # write csv
     csv_path = per_region_output_prefix / region_type / 'csv'
     csv_path.mkdir(parents=True, exist_ok=True)
     for geoid in geoid_list:
         con.execute(f"""COPY (SELECT * EXCLUDE geometry FROM {region_type}_grouped_risk WHERE GEOID = '{geoid}') TO '{csv_path}/{geoid}.csv.gz'
-        (FORMAT CSV, OVERWRITE_OR_IGNORE);""")
+        (FORMAT CSV, COMPRESSION 'gzip', FILE_EXTENSION 'csv.gz', OVERWRITE_OR_IGNORE);""")
 
     # write geojson
     geojson_path = per_region_output_prefix / region_type / 'geojson'
