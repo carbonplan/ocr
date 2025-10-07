97e3fa6f04c6f51744c35e2c6c2749704a5bb8bc
Anderson Banihirwe <axbanihirwe@ualr.edu>
2025-10-07 10:48:12 -0700

    fix: update fire risk calculations to include wind-informed burn probabilities for 2011 and 2047

--- ocr/risks/fire.py
+++ ocr/risks/fire.py
@@ -452,18 +452,32 @@ def calculate_wind_adjusted_risk(
         unburnable_mask_climate_run=unburnable_mask_subset,
     )
 
-    # Start fire_risk dataset with USFS RPS (risk to potential structures) baseline
-    fire_risk = (rps_30_subset['RPS']).to_dataset(name='USFS_RPS')
+    # wind_risk_2011 (our wind-informed RPS value)
+    fire_risk = (wind_informed_bp_corrected_2011).to_dataset(name='wind_risk_2011')
 
-    fire_risk['wind_risk_2011'] = wind_informed_bp_corrected_2011 * rps_30_subset['CRPS']
-    fire_risk['wind_risk_2047'] = wind_informed_bp_corrected_2047 * rps_30_subset['CRPS']
+    # wind_risk_2047 (our wind-informed RPS value)
+    fire_risk['wind_risk_2047'] = wind_informed_bp_corrected_2047
 
-    # Add metadata/attrs to the variables in the dataset
-    # BP is burn probability (should be between 0 and 1) and CRPS is the conditional risk to potential structures - aka "if a structure burns, how bad would it be"
-    for var in fire_risk.data_vars:
-        fire_risk[var].attrs['units'] = 'dimensionless'
-        fire_risk[var].attrs['long_name'] = f'{var} (wind-adjusted)'
-        fire_risk[var].attrs['description'] = f'{var} adjusted for wind effects'
+    # burn_probability_2011 (our wind-informed BP value)
+    fire_risk['burn_probability_2011'] = wind_informed_bp_corrected_2011 * rps_30_subset['CRPS']
+    fire_risk['burn_probability_2011'].attrs['description'] = (
+        'Wind-informed burn probability for 2011 calculated as wind-informed BP * CRPS'
+    )
+
+    # burn_probability_2047 (our wind-informed BP value)
+    fire_risk['burn_probability_2047'] = wind_informed_bp_corrected_2047 * rps_30_subset['CRPS']
+    fire_risk['burn_probability_2047'].attrs['description'] = (
+        'Wind-informed burn probability for 2047 calculated as wind-informed BP * CRPS'
+    )
+
+    # conditional_risk (from USFS Scott 2024)(RDS-2020-0016-2)
+    fire_risk['conditional_risk_usfs'] = rps_30_subset['CRPS']
+
+    # burn_probability_usfs_2011 (BP from Riley 2025 (RDS-2025-0006))
+    fire_risk['burn_probability_usfs_2011'] = climate_run_2011_subset['BP']
+
+    # burn_probability_usfs_2047 (BP from Riley 2025 (RDS-2025-0006))
+    fire_risk['burn_probability_usfs_2047'] = climate_run_2047_subset['BP']
 
     return fire_risk.drop_vars(['spatial_ref', 'crs', 'quantile'], errors='ignore')
 
