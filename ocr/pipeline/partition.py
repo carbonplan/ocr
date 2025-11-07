722aa7e4d168542fb3d8715a1a6d4884267f5c89
Anderson Banihirwe <axbanihirwe@ualr.edu>
2025-11-07 13:51:25 -0800

    Add consolidated buildings parquet output after partitioning

--- ocr/pipeline/partition.py
+++ ocr/pipeline/partition.py
@@ -42,3 +42,22 @@ def partition_buildings_by_geography(config: OCRConfig):
 
     if config.debug:
         console.log(f'Partitioned buildings written to: {output_path}')
+
+    consolidate_buildings_parquet = (
+        f'{config.vector.building_geoparquet_uri.parent / "consolidated-buildings.parquet"}'
+    )
+
+    if config.debug:
+        console.log(f'Creating a consolidated parquet file at: {consolidate_buildings_parquet}')
+
+    connection.execute(f"""
+        SET preserve_insertion_order=false;
+        COPY (
+        SELECT *
+        FROM '{path}')
+        TO  '{consolidate_buildings_parquet}' (
+        FORMAT 'parquet',
+        COMPRESSION 'zstd',
+        OVERWRITE_OR_IGNORE true);""")
+    if config.debug:
+        console.log(f'Consolidated buildings written to: {consolidate_buildings_parquet}')
