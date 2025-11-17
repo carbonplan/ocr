38d24345d7717cbb1d0bf47112e0a3cbff5b2719
norlandrhagen <norlandrhagen@gmail.com>
2025-11-17 15:12:10 -0700

    in building centroids, round two decimals

--- ocr/pipeline/create_building_centroid_pmtiles.py
+++ ocr/pipeline/create_building_centroid_pmtiles.py
@@ -41,8 +41,8 @@ def create_building_centroid_pmtiles(
                 SELECT
                     'Feature' AS type,
                     json_object(
-                        '0', wind_risk_2011,
-                        '1', wind_risk_2047
+                    '0', ROUND(wind_risk_2011, 2),
+                    '1', ROUND(wind_risk_2047, 2)
                     ) AS properties,
                     json(ST_AsGeoJson(ST_Centroid(geometry))) AS geometry
                 FROM read_parquet('{input_path}')
