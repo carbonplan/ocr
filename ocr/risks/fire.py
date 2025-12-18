59c64baea2ba0a9982fe25e2ed6ea479e231e19e
Anderson Banihirwe <axbanihirwe@ualr.edu>
2025-12-18 06:58:55 -0800

    Update fire risk calculations to include unit descriptions and convert burn probabilities to percentage

--- ocr/risks/fire.py
+++ ocr/risks/fire.py
@@ -634,20 +634,24 @@ def calculate_wind_adjusted_risk(
     # Note: for QA. Remove in further production versions
     rps_30_subset = rps_30.sel(latitude=y_slice, longitude=x_slice)
     fire_risk['USFS_RPS'] = rps_30_subset['RPS']
+    fire_risk['USFS_RPS'].attrs['description'] = 'RPS from USFS Scott et al. (2024) RDS-2020-0016-2'
+    fire_risk['USFS_RPS'].attrs['units'] = 'percent'
 
     with xr.set_options(arithmetic_join='exact'):
         # wind_risk_2011 (our wind-informed RPS value)
         # Divide by 100 to return RPS as a percentage
-        fire_risk['wind_risk_2011'] = wind_informed_bp_combined_2011 * rps_30_subset['CRPS'] / 100
+        fire_risk['wind_risk_2011'] = (wind_informed_bp_combined_2011 * rps_30_subset['CRPS']) / 100
         fire_risk['wind_risk_2011'].attrs['description'] = (
             'Wind-informed RPS for 2011 calculated as wind-informed BP * CRPS'
         )
+        fire_risk['wind_risk_2011'].attrs['units'] = 'percent'
         # wind_risk_2047 (our wind-informed RPS value)
         # Divide BP by 100 to convert from percentage back to decimal for RPS calculation
-        fire_risk['wind_risk_2047'] = (wind_informed_bp_combined_2047 / 100) * rps_30_subset['CRPS']
+        fire_risk['wind_risk_2047'] = (wind_informed_bp_combined_2047 * rps_30_subset['CRPS']) / 100
         fire_risk['wind_risk_2047'].attrs['description'] = (
             'Wind-informed RPS for 2047 calculated as wind-informed BP * CRPS'
         )
+        fire_risk['wind_risk_2047'].attrs['units'] = 'percent'
 
         # burn_probability_2011 (our wind-informed BP value)
         fire_risk['burn_probability_2011'] = wind_informed_bp_combined_2011
@@ -665,12 +669,24 @@ def calculate_wind_adjusted_risk(
 
         # conditional_risk (from USFS Scott 2024)(RDS-2020-0016-2)
         fire_risk['conditional_risk_usfs'] = rps_30_subset['CRPS']
+        fire_risk['conditional_risk_usfs'].attrs['description'] = (
+            'Conditional Risk from USFS Scott et al. (2024) RDS-2020-0016-2'
+        )
+        fire_risk['conditional_risk_usfs'].attrs['units'] = 'percent'
 
         # burn_probability_usfs_2011 (BP from Riley 2025 (RDS-2025-0006))
-        fire_risk['burn_probability_usfs_2011'] = riley_2011_30m_4326_subset['BP']
+        fire_risk['burn_probability_usfs_2011'] = riley_2011_30m_4326_subset['BP'] * 100
+        fire_risk['burn_probability_usfs_2011'].attrs['description'] = (
+            'Burn Probability from USFS Riley et al. (2025) RDS-2025-0006 for year 2011'
+        )
+        fire_risk['burn_probability_usfs_2011'].attrs['units'] = 'percent'
 
         # burn_probability_usfs_2047 (BP from Riley 2025 (RDS-2025-0006))
-        fire_risk['burn_probability_usfs_2047'] = riley_2047_30m_4326_subset['BP']
+        fire_risk['burn_probability_usfs_2047'] = riley_2047_30m_4326_subset['BP'] * 100
+        fire_risk['burn_probability_usfs_2047'].attrs['description'] = (
+            'Burn Probability from USFS Riley et al. (2025) RDS-2025-0006 for year 2047'
+        )
+        fire_risk['burn_probability_usfs_2047'].attrs['units'] = 'percent'
 
     return fire_risk.drop_vars(['spatial_ref', 'crs', 'quantile'], errors='ignore')
 
